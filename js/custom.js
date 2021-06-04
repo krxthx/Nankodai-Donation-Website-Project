268ba4e90836ce55cc4537323c8a4836a077a078
unknown <krithika.23it@licet.ac.in>
2021-06-04 20:57:27 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+/*
+#TheSparksFoundationInternship
+#GripJune21
+#WebDevInternship
+#Task3
+#PaymentGatewayIntegration	
+#KrithikaNithyanandam
+*/
+
+$(document).ready(function() {
+	$('#HomePage').css({height:$(window).height(),width:$(window).width()});
+	$(window).resize(function(){
+        $('#HomePage').css({height:$(window).height(),width:$(window).width()});
+    });
+
+});
+
